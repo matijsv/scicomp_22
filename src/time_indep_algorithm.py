115451fa4fc33dff09b2f39881bce5a5f2f343a7
HarvestStars <574817827@qq.com>
2025-02-07 13:13:45 +0100

    add gauss_seidel_wavefront algorithm, it is a parallel implementation

--- src/time_indep_algorithm.py
+++ src/time_indep_algorithm.py
@@ -100,7 +100,6 @@ def compute_max_diff(c_k, c_kp1):
 def gauss_seidel_seq(N=50, M=50, D=1.0, dt=0.0001, dx=1/50, max_iterations=50000, epsilon=1e-5):
     """
     Gauss-Seidel interation for 2D diffusion equation with periodic boundary conditions.
-    This is for Elliptic PDE: ∂c/∂t = D(∂^2c/∂x^2 + ∂^2c/∂y^2)
 
     Parameters:
     -----------
@@ -162,6 +161,72 @@ def gauss_seidel_seq(N=50, M=50, D=1.0, dt=0.0001, dx=1/50, max_iterations=50000
 
     return c_k, iteration, delta
 
+@njit(parallel=True)
+def gauss_seidel_wavefront(N=50, M=50, D=1.0, dt=0.0001, dx=1/50, max_iterations=50000, epsilon=1e-5):
+    """
+    Gauss-Seidel parallel implementation, known as "wave front", for 2D diffusion equation with periodic boundary conditions.
+
+    Parameters:
+    -----------
+    N : int
+        Number of grid points in x-direction
+    M : int
+        Number of grid points in y-direction
+    D : float
+        Diffusion coefficient, default to 1.0
+    dt : float
+        Time step size
+    dx : float
+        Grid spacing
+    max_iterations : int
+        Maximum number of iterations
+    epsilon : float
+        Convergence criterion
+
+    Returns:
+    --------
+    c_k : 2D array
+        Concentration distribution at time step k
+    iteration : int
+        Number of iterations
+    delta : float
+        Error at convergence
+    """
+    # initialize the concentration array
+    c_k = np.zeros((N, M))
+
+    # set the left and right boundary values
+    left_right_boundary = np.linspace(0, 1, N)
+
+    # stationary boundary conditions
+    c_k[:, 0] = left_right_boundary
+    c_k[:, -1] = left_right_boundary
+    c_k[-1, :] = 1
+    c_k[0, :] = 0
+
+    alpha = D * dt / (dx ** 2)
+
+    iteration = 0
+    while iteration < max_iterations:
+        delta = 0.0
+
+        # wavefront update, parallel processing
+        # the wavefront is the diagonal line from right-top to left-bottom
+        for wavefront in range(2, N + M - 2):  # wavefront index, recall that the first and last rows and columns are boundary, which are fixed. so we should -2 after N+M
+            for i in prange(1, N - 1):  # parallel processing
+                j = wavefront - i
+                if 1 <= j < M - 1: # we should skip j = M-1, because it is the boundary
+                    old_value = c_k[i, j]
+                    c_k[i, j] = alpha * (c_k[i+1, j] + c_k[i-1, j] + c_k[i, j+1] + c_k[i, j-1])
+                    delta = max(delta, abs(c_k[i, j] - old_value))
+
+        # check for convergence
+        if delta < epsilon:
+            break
+
+        iteration += 1
+
+    return c_k, iteration, delta
 
 if __name__ == "__main__":
     # run the Jacobi iteration
@@ -171,7 +236,7 @@ if __name__ == "__main__":
     #     np.allclose(optimized_concentration[:, i], optimized_concentration[:, -i])
     #print("All columns are the same symetrically.")
 
-    optimized_concentration, iteration, delta = gauss_seidel_seq()
+    optimized_concentration, iteration, delta = gauss_seidel_wavefront()
     print(f"Converged after {iteration} iterations with error {delta:.6e}")
     
     # plot the heatmap of the optimized concentration
